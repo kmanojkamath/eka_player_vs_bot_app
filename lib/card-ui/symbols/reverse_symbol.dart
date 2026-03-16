cdc219f02704c3cc442fb520a11c7bfbbcad6c02
K Manoj Kamath <kmanojkamath@gmail.com>
2026-03-16 12:26:26 +0530

    Added Documentation

--- lib/card-ui/symbols/reverse_symbol.dart
+++ lib/card-ui/symbols/reverse_symbol.dart
@@ -3,27 +3,31 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ReverseSymbol extends CustomPainter {
+  //CustomPainter to draw the reverse symbol on reverse cards
   @override
   void paint(Canvas canvas, Size size) {
-    double x = size.width / 4.2;
+    double x = size.width / 4.2; //Base unit for symbol dimensions
 
+    //Rotate the canvas to draw the reverse symbol at an angle
     canvas.translate(size.width / 2, size.height / 2);
     canvas.rotate(-0.6);
     canvas.translate(-size.width / 2, -size.height / 2);
 
     canvas.drawArc(
+      //Draw the upper quarter of the reverse symbol
       Rect.fromCenter(
         center: Offset(2.3 * x, size.height / 2 - 0.1 * x),
         width: 1.8 * x,
         height: 1.8 * x,
       ),
       pi,
-      pi * 1/2,
+      pi * 1 / 2,
       true,
       Paint()..color = Colors.white,
     );
 
     canvas.drawRRect(
+      //Draw the upper rectangle of the reverse symbol
       RRect.fromLTRBR(
         2.3 * x,
         size.height / 2 - 1 * x,
@@ -34,7 +38,8 @@ class ReverseSymbol extends CustomPainter {
       Paint()..color = Colors.white,
     );
 
-    final upperTrianglePath = Path();
+    final upperTrianglePath =
+        Path(); //Path to draw the upper triangle of the reverse symbol
     upperTrianglePath.moveTo(size.width, size.height / 2 - 0.55 * x);
     upperTrianglePath.lineTo(
       size.width - x,
@@ -46,17 +51,25 @@ class ReverseSymbol extends CustomPainter {
     );
     upperTrianglePath.close();
 
-    canvas.drawPath(upperTrianglePath, Paint()..color = Colors.white);
+    canvas.drawPath(
+      upperTrianglePath,
+      Paint()..color = Colors.white,
+    ); //Draw the upper triangle of the reverse symbol
 
-    final lowerTrianglePath = Path();
+    final lowerTrianglePath =
+        Path(); //Path to draw the lower triangle of the reverse symbol
     lowerTrianglePath.moveTo(0, size.height / 2 + 0.55 * x);
     lowerTrianglePath.lineTo(x, size.height / 2 + (0.55 + 0.81) * x);
     lowerTrianglePath.lineTo(x, size.height / 2 + (0.55 - 0.81) * x);
     lowerTrianglePath.close();
 
-    canvas.drawPath(lowerTrianglePath, Paint()..color = Colors.white);
+    canvas.drawPath(
+      lowerTrianglePath,
+      Paint()..color = Colors.white,
+    ); //Draw the lower triangle of the reverse symbol
 
     canvas.drawRRect(
+      //Draw the lower rectangle of the reverse symbol
       RRect.fromLTRBR(
         x,
         size.height / 2 + 0.1 * x,
@@ -68,6 +81,7 @@ class ReverseSymbol extends CustomPainter {
     );
 
     canvas.drawArc(
+      //Draw the lower quarter of the reverse symbol
       Rect.fromCenter(
         center: Offset(1.9 * x, size.height / 2 + 0.1 * x),
         width: 1.8 * x,
--- lib/card-ui/symbols/wild_symbol.dart
+++ lib/card-ui/symbols/wild_symbol.dart
@@ -6,64 +6,71 @@ import 'dart:math';
 import 'package:eka_player_vs_bot/card-ui/card_logic.dart';
 
 class WildSymbol extends CustomPainter {
+  //CustomPainter to draw the wild symbol on wild cards
   @override
   void paint(Canvas canvas, Size size) {
+    //Rotate the canvas to draw the wild symbol at an angle
     canvas.translate(size.width / 2, size.height / 2);
     canvas.rotate(0.6);
     canvas.translate(-size.width / 2, -size.height / 2);
 
     canvas.drawOval(
+      //Draw the white oval background of the wild symbol
       Rect.fromCenter(
         center: Offset(size.width / 2, size.height / 2),
-        width: size.width*0.81,
+        width: size.width * 0.81,
         height: size.height,
       ),
       Paint()..color = Colors.white,
     );
 
     canvas.drawArc(
+      //Draw the blue quarter of the wild symbol
       Rect.fromCenter(
         center: Offset(size.width / 2, size.height / 2),
-        width: size.width * 0.95*0.81,
+        width: size.width * 0.95 * 0.81,
         height: size.height * 0.95,
       ),
-      0-0.21,
+      0 - 0.21,
       pi / 2,
       true,
       Paint()..color = color(CardColor.blue),
     );
 
     canvas.drawArc(
+      //Draw the yellow quarter of the wild symbol
       Rect.fromCenter(
         center: Offset(size.width / 2, size.height / 2),
-        width: size.width * 0.95*0.81,
+        width: size.width * 0.95 * 0.81,
         height: size.height * 0.95,
       ),
-      pi / 2-0.21,
+      pi / 2 - 0.21,
       pi / 2,
       true,
       Paint()..color = color(CardColor.yellow),
     );
 
     canvas.drawArc(
+      //Draw the red quarter of the wild symbol
       Rect.fromCenter(
         center: Offset(size.width / 2, size.height / 2),
-        width: size.width * 0.95*0.81,
+        width: size.width * 0.95 * 0.81,
         height: size.height * 0.95,
       ),
-      pi-0.21,
+      pi - 0.21,
       pi / 2,
       true,
       Paint()..color = color(CardColor.red),
     );
 
     canvas.drawArc(
+      //Draw the green quarter of the wild symbol
       Rect.fromCenter(
         center: Offset(size.width / 2, size.height / 2),
-        width: size.width * 0.95*0.81,
+        width: size.width * 0.95 * 0.81,
         height: size.height * 0.95,
       ),
-      3 * pi / 2-0.21,
+      3 * pi / 2 - 0.21,
       pi / 2,
       true,
       Paint()..color = color(CardColor.green),
