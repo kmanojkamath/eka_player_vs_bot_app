bd9eee9b7716812cac98f50da008dbc489eb7ef7
K Manoj Kamath <kmanojkamath@gmail.com>
2026-03-16 10:51:42 +0530

    Add Card Storage

---
+++ lib/card/card_storage.dart
@@ -0,0 +1,13 @@
+import 'card_logic.dart';
+
+Set<EkaCard> deckPile = List.generate(108, (i) {
+  return EkaCard(i);
+}).toSet();
+
+Set<EkaCard> discardPile = {};
+
+Set<EkaCard> playerPile = {};
+
+Set<EkaCard> botPile = {};
+
+late EkaCard topCard;
