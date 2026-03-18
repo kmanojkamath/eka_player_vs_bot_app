e5912899cc0a784e0b4bc5c07589ae8d6f26da0c
K Manoj Kamath <kmanojkamath@gmail.com>
2026-03-18 14:44:32 +0530

    Update scale feature for turn illusion

--- lib/card/card-backend/card_storage.dart
+++ lib/card/card-backend/card_storage.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 import '../card_logic.dart';
 
 List<EkaCard> card = List.generate(108, (i) {
   if (1 < 100) {
-    return EkaCard(CardColor.values[i ~/ 25], i % 25);
+    return EkaCard(CardColor.values[i ~/ 25], (i % 25 + 1) ~/ 2);
   } else if (i < 104) {
     return EkaCard(CardColor.wild, 13);
   } else {
@@ -10,15 +12,20 @@ List<EkaCard> card = List.generate(108, (i) {
   }
 }); // Generate a standard deck of 108 cards
 
-Set<int> deckPile = List.generate(108, (i) {
-  return i;
-}).toSet(); // Set to hold the indices of the cards in the deck pile, initialized with all card indices from 0 to 107
+Set<int> deckPile = List.generate(
+  108,
+  (i) {
+    return i;
+  },
+).toSet(); // Set to hold the indices of the cards in the deck pile, initialized with all card indices from 0 to 107
 
 Set<int> discardPile =
     {}; // Set to hold the indices of the cards in the discard pile, top card will be the last card added to this set
 
-Set<int> playerPile = {}; // Set to hold the indices of the cards in the player's pile
+Set<int> playerPile =
+    {}; // Set to hold the indices of the cards in the player's pile
 
 Set<int> botPile = {}; // Set to hold the indices of the cards in the bot's pile
 
-late int topCard; // Variable to hold the index of the top card of the discard pile
+late int
+topCard; // Variable to hold the index of the top card of the discard pile
