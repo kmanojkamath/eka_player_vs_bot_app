aab8691ef30939bc7413b393a1f3fdf95f9c4038
K Manoj Kamath <kmanojkamath@gmail.com>
2026-03-18 14:46:15 +0530

    Added Animations to Hand View

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:eka_player_vs_bot/screens/card_animation_test_screen2.dart';
+import 'package:eka_player_vs_bot/screens/hand_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,6 +18,6 @@ class MyApp extends StatelessWidget {
     - CardScaleAnimationScreen(): A screen to test scaling, positioning, and rotation animations on a card.
     - CardAnimationScreen(): A screen to test a sequence of animations on a card, including moving, rotating, and scaling.
     */
-    return MaterialApp(home:CardAnimationScreen());
+    return MaterialApp(home:HandViewScreen());
   }
 }
--- lib/screens/hand_view.dart
+++ lib/screens/hand_view.dart
@@ -1,44 +1,58 @@
 import 'dart:math';
 
+import 'package:eka_player_vs_bot/card/animated-cards/animated_card.dart';
+import 'package:eka_player_vs_bot/card/card-backend/card_storage.dart';
 import 'package:flutter/material.dart';
 
-import 'package:eka_player_vs_bot/card/card_logic.dart';
-import 'package:eka_player_vs_bot/card/card-ui/card_widget.dart';
-
 double cardAngle(int n, int i) {
-  double angle = 0.2;
-  return (i - n / 2 + 0.5) * angle;
+  double angle = 0.2 * (1 - (n - 1) / 27);
+  return (i - (n - 1) / 2) * angle;
 }
 
-double cardLeftPosition(int n, int i, BuildContext context, double cardWidth) {
-  return cardCenterPosition(n, i, context) -
-      cardWidth * cos(cardAngle(n, i)) * 0.5;
+Offset cardPosition(int n, int i, BuildContext context, double cardScale) {
+  double widthDifference = 18;
+  Size size = MediaQuery.sizeOf(context);
+
+  double lowest = size.height * 0.5;
+  double highest = size.height * 0.4;
+
+  double x = i - (n - 1) / 2;
+
+  double cardWidth = 188 * cardScale;
+
+  return Offset(
+    x * widthDifference +
+        size.width / 2 -
+        cardWidth * cos(cardAngle(n, i)) * 0.5,
+    (lowest - highest) / ((n / 2) * (n / 2)) * x * x +
+        highest,
+  );
 }
 
-double cardCenterPosition(int n, int i, BuildContext context) {
-  double widthDifference = 10;
-  return ((i - n / 2 + 0.5) * widthDifference +
-      MediaQuery.sizeOf(context).width / 2);
+class HandView extends StatefulWidget {
+  List<CardController> cardController = List.generate(
+    25,
+    (i) => CardController(),
+  );
+  HandView({super.key});
+
+  @override
+  State<HandView> createState() => _HandViewState();
 }
 
-class HandView extends StatelessWidget {
-  final int n;
-  const HandView(this.n, {super.key});
-
+class _HandViewState extends State<HandView> {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: List.generate(n, (i) {
-        return Positioned(
-          left: cardLeftPosition(n, i, context, 150 * 188 / 281),
-          bottom: MediaQuery.sizeOf(context).height * 0.05,
-          child: Transform.rotate(
-            alignment: Alignment.bottomCenter,
-            angle: cardAngle(n, i),
-            child: EkaCardWidget(
-              EkaCard(CardColor.values[i ~/ 25], i % 25),
-              cardScale: 0.55,
-            ),
+      children: List.generate(25, (i) {
+        return AnimatedCard(
+          card[i*2],
+          widget.cardController[i],
+          cardScale: MediaQuery.sizeOf(context).height * 0.1 / 281,
+          cardWidthScale: 0,
+          cardPosition: Offset(
+            MediaQuery.sizeOf(context).width * 0.75,
+            MediaQuery.sizeOf(context).height * 0.1,
           ),
         );
       }),
@@ -54,23 +68,58 @@ class HandViewScreen extends StatefulWidget {
 }
 
 class _HandViewScreenState extends State<HandViewScreen> {
-  int n = 1;
+  int i = 0;
   bool front = true;
+  final hv = HandView();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HandView(n),
+      body: hv,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
           setState(() {
-            if (n == 13) front = false;
-            if (n == 1) front = true;
+            if (i == 25) front = false;
+            if (i == 1) front = true;
             if (front) {
-              n++;
+              i++;
             } else {
-              n--;
+              i--;
             }
           });
+
+          await Future.wait([
+            hv.cardController[i].changeWidthScale!.call(
+              1,
+              Duration(milliseconds: 500),
+              Curves.linear,
+            ),
+            hv.cardController[i].changeScale!.call(
+              MediaQuery.sizeOf(context).height * 0.25 / 281,
+              Duration(milliseconds: 500),
+              Curves.easeInOut,
+            ),
+          ]);
+
+          List<Future> L = [];
+          L.addAll(
+            List.generate(i + 1, (j) {
+              return hv.cardController[j].changeAngle!.call(
+                cardAngle(i + 2, j),
+                Duration(milliseconds: j == i ? 750 : 400),
+                Curves.linear,
+              );
+            }),
+          );
+          L.addAll(
+            List.generate(i + 1, (j) {
+              return hv.cardController[j].changePosition!.call(
+                cardPosition(i + 2, j, context, 0.5),
+                Duration(milliseconds: j == i ? 750 : 400),
+                Curves.linear,
+              );
+            }),
+          );
+          await Future.wait(L as Iterable<Future<dynamic>>);
         },
       ),
     );
