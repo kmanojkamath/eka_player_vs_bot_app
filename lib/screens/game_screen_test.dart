a9ff5aa4ccfcf196e86691a67fe37884503e3e29
K Manoj Kamath <kmanojkamath@gmail.com>
2026-03-24 23:04:35 +0530

    Added Result Screen

--- lib/screens/game_screen_test.dart
+++ lib/screens/game_screen_test.dart
@@ -1,6 +1,7 @@
 import 'package:eka_player_vs_bot/card/animated-cards/bot_cards_holder.dart';
 import 'package:eka_player_vs_bot/card/animated-cards/top_card.dart';
 import 'package:eka_player_vs_bot/game_logic/game_start.dart';
+import 'package:eka_player_vs_bot/screens/result_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:eka_player_vs_bot/global.dart';
 
@@ -18,6 +19,12 @@ class _GameScreenState extends State<GameScreen> {
   @override
   void initState() {
     super.initState();
+    showResultScreen = (bool didWin) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => ResultScreen(didWin: didWin)),
+      );
+    };
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await gameStart();
     });
@@ -27,7 +34,14 @@ class _GameScreenState extends State<GameScreen> {
   Widget build(BuildContext context) {
     screenSize = MediaQuery.sizeOf(context);
     return Scaffold(
-      body: Stack(children: [TopCard(),BotCardsHolder(), PlayerCardsHolder(), ColorSelector()]),
+      body: Stack(
+        children: [
+          TopCard(),
+          BotCardsHolder(),
+          PlayerCardsHolder(),
+          ColorSelector(),
+        ],
+      ),
     );
   }
 }
