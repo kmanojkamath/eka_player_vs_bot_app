3371fd99dc7a49fbde8974a1faf7a5120c249149
K Manoj Kamath <kmanojkamath@gmail.com>
2026-03-24 22:27:40 +0530

    Added CardColorSelector. (Fix is still required)

--- lib/logics/medium_bot.dart
+++ lib/logics/medium_bot.dart
@@ -1,12 +1,29 @@
 import 'dart:math';
 
+import '../card/card_logic.dart';
+import '../global.dart';
 import 'playable_cards.dart';
 
 Future<int> mediumBot() async {
   // Function to determine the Medium bot's move
-  if(playableBotCards().isEmpty) {
+  if (playableBotCards().isEmpty) {
     return -1;
   } else {
-    return Random().nextInt(playableBotCards().length);
+    return playableBotCards().elementAt(
+      Random().nextInt(playableBotCards().length),
+    );
+  }
+}
+
+Future<CardColor> mediumBotColor() async {
+  Set<CardColor> colors = {};
+  playableBotCards().forEach((ci) {
+    colors.add(card[ci].color);
+  });
+  colors.remove(CardColor.wild);
+  if (colors.isEmpty) {
+    return CardColor.values[Random().nextInt(4)];
+  } else {
+    return colors.toList().elementAt(Random().nextInt(colors.length));
   }
 }
--- lib/logics/playable_cards.dart
+++ lib/logics/playable_cards.dart
@@ -4,14 +4,7 @@ List<int> playableBotCards() {
   // Function to determine which cards in the bot's pile are playable based on the top card of the discard pile
   List<int> S = [];
   for (int ci in botPile) {
-    if (topCard.isWild &&
-        (card[ci].color == selectedColor || card[ci].isWild)) {
-      S.add(ci);
-    } else if (card[ci].isWild ||
-        card[ci].color == topCard.color ||
-        card[ci].value == topCard.value) {
-      S.add(ci);
-    }
+    if (isPlayable(ci)) S.add(ci);
   }
   return S;
 }
@@ -26,7 +19,7 @@ List<int> playablePlayerCards() {
 }
 
 bool isPlayable(int ci) {
-  if (topCard.isWild && (card[ci].color == selectedColor || card[ci].isWild)) {
+  if (topCard.isWild && (card[ci].color == selectedColor.value || card[ci].isWild)) {
     return true;
   } else if (card[ci].isWild ||
       card[ci].color == topCard.color ||
